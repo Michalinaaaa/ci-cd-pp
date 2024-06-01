18309805898da0196cfc9c4a398f2223d746db3e
Michalinaaaa <136478286+Michalinaaaa@users.noreply.github.com>
2024-06-01 18:15:47 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,51 @@
 def add(a: int, b: int) -> int:
+    """
+    Function to add two numbers.
+
+    Parameters:
+        a (int): The first number.
+        b (int): The second number.
+
+    Returns:
+        int: The sum of a and b.
+    """
     return a + b
 
 def subtract(a: int, b: int) -> int:
+    """
+    Function to subtract two numbers.
+
+    Parameters:
+        a (int): The first number.
+        b (int): The second number.
+
+    Returns:
+        int: The result of subtracting b from a.
+    """
     return a - b
 
 def multiply(a: int, b: int) -> int:
+    """
+    Function to multiply two numbers.
+
+    Parameters:
+        a (int): The first number.
+        b (int): The second number.
+
+    Returns:
+        int: The product of a and b.
+    """
     return a * b
 
 def divide(a: int, b: int) -> float:
+    """
+    Function to divide two numbers.
+
+    Parameters:
+        a (int): The numerator.
+        b (int): The denominator.
+
+    Returns:
+        float: The result of dividing a by b.
+    """
     return a / b
